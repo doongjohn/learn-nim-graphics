8479b315097495900246136c318f22c341fdb5b2
doongjohn <doongjohn@gmail.com>
2021-01-31 12:53:26 +0900

    test

--- git-logout.bat
+++ git-logout.bat
@@ -1,3 +1,4 @@
 git config --global --unset user.name &&^
 git config --global --unset user.email &&^
-git config --global --unset credential.helper
+git config --global --unset credential.helper &&^
+git credential-manager reject https://github.com
