cdea3b8e5a70df72096e3a4eca4160962fb2824d
doongjohn <doongjohn@gmail.com>
2021-02-01 15:39:12 +0900

    update git helpers

--- git-logout.bat
+++ git-logout.bat
@@ -1,8 +1,6 @@
-(
-    git config --global --unset user.name
-    git config --global --unset user.email
-    git config --global --unset credential.helper
-    git config --system --unset credential.helper
-)
+git config --global --unset user.name
+git config --global --unset user.email
+git config --global --unset credential.helper
+git config --system --unset credential.helper
 cmdkey /delete git:https://github.com
 cmdkey /delete vscodevscode.github-authentication/github.auth
